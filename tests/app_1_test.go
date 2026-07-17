f4cfafbae0d90512869a342bc55ecf1e9a05d427
dev1 <dev1@gmail.com>
2026-07-17 22:51:26 +0300

    github settings try7

--- tests/app_1_test.go
+++ tests/app_1_test.go
@@ -21,7 +21,7 @@ func getURL(path string) string {
 			port = int(eport)
 		}
 	}
-	path = strings.TrimPrefix(strings.ReplaceAll(path, `\`, `/`), `../web/`)
+	path = strings.TrimPrefix(strings.ReplaceAll(path, `\`, `/`), `./web/`)
 	return fmt.Sprintf("http://localhost:%d/%s", port, path)
 }
 
